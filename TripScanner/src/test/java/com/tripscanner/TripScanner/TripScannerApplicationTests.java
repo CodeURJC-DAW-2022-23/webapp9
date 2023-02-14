b075825fc6196a229ba6f4983aab81718080802a
Adaya:) <103607990+AdayaUwU@users.noreply.github.com>
2023-02-14 10:26:37 +0100

    Add: Maven and Spring project

---
+++ TripScanner/src/test/java/com/tripscanner/TripScanner/TripScannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripscanner.TripScanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripScannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
