4671bace9e6ab97f73652e0d749ff9a88b24b7dc
xIvqn <ivanp2002@gmail.com>
2023-02-20 19:00:04 +0100

    Feat: Itinerary service

---
+++ TripScanner/src/main/java/com/tripscanner/TripScanner/service/ItineraryService.java
@@ -0,0 +1,38 @@
+package com.tripscanner.TripScanner.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tripscanner.TripScanner.model.Itinerary;
+import com.tripscanner.TripScanner.repository.ItineraryRepository;
+
+@Service
+public class ItineraryService {
+
+    @Autowired
+    private ItineraryRepository repository;
+
+    public Optional<Itinerary> findById(long id) {
+        return repository.findById(id);
+    }
+
+    public boolean exist(long id) {
+        return repository.existsById(id);
+    }
+
+    public List<Itinerary> findAll() {
+        return repository.findAll();
+    }
+
+    public void save(Itinerary itinerary) {
+        repository.save(itinerary);
+    }
+
+    public void delete(long id) {
+        repository.deleteById(id);
+    }
+
+}
