d84e505863b94a44dd3c693e2089fa61e34ca415
xIvqn <ivanp2002@gmail.com>
2023-02-20 13:44:17 +0100

    Feat: Itinerary model

---
+++ TripScanner/src/main/java/com/tripscanner/TripScanner/model/Itinerary.java
@@ -0,0 +1,95 @@
+package com.tripscanner.TripScanner.model;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Itinerary {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id = null;
+
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    private Long views;
+
+    @ManyToMany
+    private List<Place> places;
+
+    @ManyToOne
+    private User user;
+
+    @OneToMany
+    private List<Review> reviews;
+
+    public Itinerary() {
+    }
+
+    public Itinerary(Long id, String name, String description) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getViews() {
+        return views;
+    }
+
+    public void setViews(Long views) {
+        this.views = views;
+    }
+
+    public List<Place> getPlaces() {
+        return places;
+    }
+
+    public void setPlaces(List<Place> places) {
+        this.places = places;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+}
