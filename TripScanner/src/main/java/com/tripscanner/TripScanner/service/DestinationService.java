64a129a9210fa138ad343de3c811cc7d23223d2a
xIvqn <ivanp2002@gmail.com>
2023-02-20 18:59:51 +0100

    Feat: Destination service

---
+++ TripScanner/src/main/java/com/tripscanner/TripScanner/service/DestinationService.java
@@ -0,0 +1,38 @@
+package com.tripscanner.TripScanner.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tripscanner.TripScanner.model.Destination;
+import com.tripscanner.TripScanner.repository.DestinationRepository;
+
+@Service
+public class DestinationService {
+
+    @Autowired
+    private DestinationRepository repository;
+
+    public Optional<Destination> findById(long id) {
+        return repository.findById(id);
+    }
+
+    public boolean exist(long id) {
+        return repository.existsById(id);
+    }
+
+    public List<Destination> findAll() {
+        return repository.findAll();
+    }
+
+    public void save(Destination destination) {
+        repository.save(destination);
+    }
+
+    public void delete(long id) {
+        repository.deleteById(id);
+    }
+
+}
