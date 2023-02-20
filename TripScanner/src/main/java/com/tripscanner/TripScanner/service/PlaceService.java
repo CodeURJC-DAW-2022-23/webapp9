7297ba857c253704a70803b288141e3f97b1aba7
xIvqn <ivanp2002@gmail.com>
2023-02-20 19:00:15 +0100

    Feat: Place service

---
+++ TripScanner/src/main/java/com/tripscanner/TripScanner/service/PlaceService.java
@@ -0,0 +1,38 @@
+package com.tripscanner.TripScanner.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tripscanner.TripScanner.model.Place;
+import com.tripscanner.TripScanner.repository.PlaceRepository;
+
+@Service
+public class PlaceService {
+
+    @Autowired
+    private PlaceRepository repository;
+
+    public Optional<Place> findById(long id) {
+        return repository.findById(id);
+    }
+
+    public boolean exist(long id) {
+        return repository.existsById(id);
+    }
+
+    public List<Place> findAll() {
+        return repository.findAll();
+    }
+
+    public void save(Place place) {
+        repository.save(place);
+    }
+
+    public void delete(long id) {
+        repository.deleteById(id);
+    }
+
+}
