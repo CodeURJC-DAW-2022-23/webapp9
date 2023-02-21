0a50ad9282d5efcfbcc3ae050ccceeb45a4e6237
xIvqn <ivanp2002@gmail.com>
2023-02-21 19:30:55 +0100

    Feat: Flag setter for db

--- TripScanner/src/main/java/com/tripscanner/TripScanner/service/DatabaseInitializer.java
+++ TripScanner/src/main/java/com/tripscanner/TripScanner/service/DatabaseInitializer.java
@@ -72,4 +72,13 @@ public class DatabaseInitializer {
 
     }
 
+    public void setFlag(Destination destination, String classpathResource) throws IOException {
+
+        Resource flag = new ClassPathResource(classpathResource);
+
+        destination.setImageFile(BlobProxy.generateProxy(flag.getInputStream(), flag.contentLength()));
+        destination.setImage(true);
+
+    }
+
 }
