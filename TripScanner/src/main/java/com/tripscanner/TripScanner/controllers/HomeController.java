752e085171f3fb4b482e279f2d40f3613700e455
anna-trofimova <cobrana1000@gmail.com>
2023-02-23 12:22:07 +0100

    Update:HomePage
    Added method to sort most visited destinations

--- TripScanner/src/main/java/com/tripscanner/TripScanner/controllers/HomeController.java
+++ TripScanner/src/main/java/com/tripscanner/TripScanner/controllers/HomeController.java
@@ -10,6 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 @Controller
 public class HomeController {
@@ -25,15 +27,30 @@ public class HomeController {
     @GetMapping("/home")
     public String showHomePage(Model model) {
 
-
-        // Show the most popular destinations
         ArrayList<Destination> destinations = (ArrayList<Destination>) destinationService.findAll();
+
+        // Sort destinations by views (rating)
+        Collections.sort(destinations, new Comparator<Destination>() {
+            @Override
+            public int compare(Destination o1, Destination o2) {
+                if (o1.getViews() < o2.getViews()) {
+                    return 1;
+                } else if (o1.getViews() > o2.getViews()) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            }
+        });
+        // Show the most visited destinations
         for (int i = 0; i < 5; i++) {
             model.addAttribute("popularDestination", destinations.get(i));
         }
+
         model.addAttribute("destination", destinationService.findAll());
         model.addAttribute("place", placeService.findAll());
         model.addAttribute("itinerary", itineraryService.findAll());
+
         return "home";
     }
 
