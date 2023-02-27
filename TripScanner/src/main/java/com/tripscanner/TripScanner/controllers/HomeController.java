efe42d56bd78d646322e8def8dba320bccd06286
anna-trofimova <cobrana1000@gmail.com>
2023-02-27 15:56:34 +0100

    Fixed: sort of popular destinations

--- TripScanner/src/main/java/com/tripscanner/TripScanner/controllers/HomeController.java
+++ TripScanner/src/main/java/com/tripscanner/TripScanner/controllers/HomeController.java
@@ -7,6 +7,7 @@ import com.tripscanner.TripScanner.service.DestinationService;
 import com.tripscanner.TripScanner.service.PlaceService;
 import com.tripscanner.TripScanner.service.ItineraryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -29,24 +31,11 @@ public class HomeController {
     @GetMapping("/")
     public String showHomePage(Model model) {
 
-        ArrayList<Destination> destinations = (ArrayList<Destination>) destinationService.findAll();
+        List<Destination> popularDestination = destinationService.findAll(Sort.by("views"));
 
-        // Sort destinations by views (rating)
-        Collections.sort(destinations, new Comparator<Destination>() {
-            @Override
-            public int compare(Destination o1, Destination o2) {
-                if (o1.getViews() < o2.getViews()) {
-                    return 1;
-                } else if (o1.getViews() > o2.getViews()) {
-                    return -1;
-                } else {
-                    return 0;
-                }
-            }
-        });
         // Show the most visited destinations
         for (int i = 0; i < 5; i++) {
-            model.addAttribute("popularDestination", destinations.get(i).getName());
+            model.addAttribute("popularDestination", popularDestination.get(i));
         }
 
         // Show 3 cards with possible destinations
