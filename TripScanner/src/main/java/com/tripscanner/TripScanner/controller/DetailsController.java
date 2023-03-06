2cbb2b5d1a8f31e5ad83cfbf471ba5f33ff1a4cf
Adaya:) <103607990+AdayaUwU@users.noreply.github.com>
2023-03-06 13:30:35 +0100

    Fix: Details form without token

--- TripScanner/src/main/java/com/tripscanner/TripScanner/controller/DetailsController.java
+++ TripScanner/src/main/java/com/tripscanner/TripScanner/controller/DetailsController.java
@@ -109,9 +109,6 @@ public class DetailsController {
         itinerary.get().setViews(itinerary.get().getViews() + 1);
         itineraryService.save(itinerary.get());
 
-        itinerary.get().setViews(itinerary.get().getViews() + 1);
-        itineraryService.save(itinerary.get());
-
         return "details";
     }
 
