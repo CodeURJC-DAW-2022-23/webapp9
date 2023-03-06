efad0c3e2041961cc71ee8798be534c1c35abc3c
Adaya:) <103607990+AdayaUwU@users.noreply.github.com>
2023-03-06 13:30:56 +0100

    Feat: Added url permissions

--- TripScanner/src/main/java/com/tripscanner/TripScanner/security/WebSecurityConfig.java
+++ TripScanner/src/main/java/com/tripscanner/TripScanner/security/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -37,9 +38,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().antMatchers("/").permitAll();
         http.authorizeRequests().antMatchers("/login").permitAll();
         http.authorizeRequests().antMatchers("/sign").permitAll();
+        http.authorizeRequests().antMatchers("/details/**").permitAll();
 
         // Private pages
         http.authorizeRequests().antMatchers("/profile").hasAnyRole("USER");
+        http.authorizeRequests().antMatchers("/itinerary/add/place/**").hasAnyRole("USER");
+        http.authorizeRequests().antMatchers("/management/**").hasAnyRole("ADMIN");
+        http.authorizeRequests().antMatchers("/myItineraries/**").hasAnyRole("USER");
         // LEFT TO ADD MORE WHEN MERGE TO DEV
 
         // Login form
