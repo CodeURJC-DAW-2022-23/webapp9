dfdf901adc389f3b02cf951eaaac99b8959a2ffb
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2023-03-16 17:28:35 +0100

    Feature: Login controller for API Rest

--- TripScanner/src/main/java/com/tripscanner/TripScanner/controller/restController/RestLoginController.java
+++ TripScanner/src/main/java/com/tripscanner/TripScanner/controller/restController/RestLoginController.java
@@ -1,14 +1,36 @@
-/*package com.tripscanner.TripScanner.controller.restController;
+package com.tripscanner.TripScanner.controller.restController;
 
+import com.tripscanner.TripScanner.security.jwt.AuthResponse;
+import com.tripscanner.TripScanner.security.jwt.LoginRequest;
+import com.tripscanner.TripScanner.security.jwt.UserLoginService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/api/auth")
 public class RestLoginController {
 
     @Autowired
-    private UserLoginService userSevice;
+    private UserLoginService userService;
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(
+            @CookieValue(name = "accessToken", required = false) String accessToken,
+            @CookieValue(name = "refreshToken", required = false) String refreshToken,
+            @RequestBody LoginRequest loginRequest) {
+        return userService.login(loginRequest, accessToken, refreshToken);
+    }
+    @PostMapping("/refresh")
+    public ResponseEntity<AuthResponse> refreshToken(
+            @CookieValue(name = "refreshToken", required = false) String refreshToken) {
+        return userService.refresh(refreshToken);
+    }
+    @PostMapping("/logout")
+    public ResponseEntity<AuthResponse> logOut(HttpServletRequest request, HttpServletResponse response) {
+        return ResponseEntity.ok(new AuthResponse(AuthResponse.Status.SUCCESS, userService.logout(request, response)));
+    }
 }
-*/
