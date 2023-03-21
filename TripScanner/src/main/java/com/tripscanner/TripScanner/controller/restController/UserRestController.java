5b6d9c84a571c2136e123255692e10689fa13882
xIvqn <xivqn@proton.me>
2023-03-21 10:27:06 +0100

    Fix: Signup process

--- TripScanner/src/main/java/com/tripscanner/TripScanner/controller/restController/UserRestController.java
+++ TripScanner/src/main/java/com/tripscanner/TripScanner/controller/restController/UserRestController.java
@@ -1,6 +1,7 @@
 package com.tripscanner.TripScanner.controller.restController;
 
 import com.tripscanner.TripScanner.model.User;
+import com.tripscanner.TripScanner.model.rest.UserDTO;
 import com.tripscanner.TripScanner.service.UserService;
 import org.hibernate.engine.jdbc.BlobProxy;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,10 +11,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+import java.net.URI;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.Arrays;
+
+import static org.springframework.web.servlet.support.ServletUriComponentsBuilder.fromCurrentRequest;
 
 @RestController
 @RequestMapping("/api/users")
@@ -26,14 +30,18 @@ public class UserRestController {
     private PasswordEncoder passwordEncoder;
 
     @PostMapping("")
-    public ResponseEntity<User> register(@RequestBody User user) {
+    public ResponseEntity<User> register(@RequestBody UserDTO userDTO) {
+        User user = new User(userDTO);
+
         if (!userService.existName(user.getUsername())) {
             user.setImage(false);
             user.setPasswordHash(passwordEncoder.encode(user.getPasswordHash()));
             user.setRoles(Arrays.asList("USER"));
 
             userService.save(user);
-            return new ResponseEntity<>(user, HttpStatus.CREATED);
+            URI location = fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+
+            return ResponseEntity.created(location).body(user);
         } else {
             return new ResponseEntity<>(HttpStatus.FORBIDDEN);
         }
