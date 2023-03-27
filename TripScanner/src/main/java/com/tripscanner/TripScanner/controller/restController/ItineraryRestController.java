d7d0b804ad3f1bfec818018a7ac9b2dbd9a99460
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2023-03-27 16:37:40 +0200

    Fix: Fixed response for image editing
    Response now contains a link to the image and the image in the body of the response @AdayaUwU @anna-trofimova

--- TripScanner/src/main/java/com/tripscanner/TripScanner/controller/restController/ItineraryRestController.java
+++ TripScanner/src/main/java/com/tripscanner/TripScanner/controller/restController/ItineraryRestController.java
@@ -442,7 +442,13 @@ public class ItineraryRestController {
         itinerary.setImageFile(BlobProxy.generateProxy(imageFile.getInputStream(), imageFile.getSize()));
         itineraryService.save(itinerary);
 
-        
+        Resource file = new InputStreamResource(imageFile.getInputStream());
+
+        String baseUrl = ServletUriComponentsBuilder.fromRequestUri(request).replacePath(null).build().toUriString();
+        URI location = new URI(baseUrl + "/api/places/" + id + "/image");
+
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_TYPE, "image/jpeg", HttpHeaders.CONTENT_LOCATION, location.toString())
+                .contentLength(itinerary.getImageFile().length()).body(file);
     }
 
     @GetMapping("/{id}")
