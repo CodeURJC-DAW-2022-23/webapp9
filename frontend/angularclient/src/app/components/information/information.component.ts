5ec76b4ebd1d052e4f03b2bd98a2322a6fbe2658
xIvqn <xivqn@proton.me>
2023-04-15 21:40:36 +0200

    Fix: Information card loading correct data

--- frontend/angularclient/src/app/components/information/information.component.ts
+++ frontend/angularclient/src/app/components/information/information.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+
 import { Information } from 'src/app/models/information.model';
+import { UserDetailsDTO } from 'src/app/models/rest/user-details-dto.model';
 import { DestinationService } from 'src/app/services/destination.service';
 import { InformationService } from 'src/app/services/information.service';
 import { ItineraryService } from 'src/app/services/itinerary.service';
+import { LogInService } from 'src/app/services/log-in.service';
 import { PlaceService } from 'src/app/services/place.service';
 
 @Component({
@@ -14,21 +16,28 @@ import { PlaceService } from 'src/app/services/place.service';
 export class InformationComponent {
 
   @Input() information!: Information;
+  @Input() owned!: boolean;
+
   service!: InformationService;
 
-  constructor(private activatedRouter: ActivatedRoute,
-              private itineraryService: ItineraryService, 
+  registered: boolean = false;
+  user: UserDetailsDTO | undefined = undefined;
+
+  constructor(private itineraryService: ItineraryService, 
               private placeService: PlaceService, 
-              private destinationService: DestinationService) {
-    activatedRouter.url.subscribe((data) => {
-      if (this.information.typeLowercase === "itinerary") this.service = this.itineraryService;
-      else if (this.information.typeLowercase === "place") this.service = this.placeService;
-      else if (this.information.typeLowercase === "destination") this.service = this.destinationService;
-    });
+              private destinationService: DestinationService, 
+              private logInService: LogInService) { }
+
+  ngOnInit() {
+    if (this.information.typeLowercase === "itinerary") this.service = this.itineraryService;
+    else if (this.information.typeLowercase === "place") this.service = this.placeService;
+    else if (this.information.typeLowercase === "destination") this.service = this.destinationService;
+
+    // Set user attributes if log in.
   }
 
   loadImage(information: Information): string {
-    return this.service.getImage(information);
+    return information.flag;
   }
 
 }
