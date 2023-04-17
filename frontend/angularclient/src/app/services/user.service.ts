8c712664103baccdac07e7a6dd8d6c602474a68b
xIvqn <xivqn@proton.me>
2023-04-17 17:33:18 +0200

    Fix: Simplified User service

--- frontend/angularclient/src/app/services/user.service.ts
+++ frontend/angularclient/src/app/services/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { UserDetailsDTO } from '../models/rest/user-details-dto.model';
-import { UserDetails } from '../models/rest/user-details.model';
 
 const BASE_URL = '/api/users';
 
@@ -12,27 +12,16 @@ export class UserService {
 
   constructor(private httpClient: HttpClient) { }
 
-  getMe(): UserDetails {
-    let user: UserDetails;
-    this.httpClient.get<UserDetailsDTO>(`${BASE_URL}/me`, { withCredentials: true }).subscribe((response) => {
-      let pages: number = Math.max(
-          (response.itineraries.numberOfElements % 10 > 0 ? 1 : 0) + Math.floor(response.itineraries.numberOfElements / 10) + 1,
-          (response.reviews.numberOfElements % 10 > 0 ? 1 : 0) + Math.floor(response.reviews.numberOfElements / 10) + 1)
+  getMe(): Observable<UserDetailsDTO> {
+    return this.httpClient.get<UserDetailsDTO>(`${BASE_URL}/me`, { withCredentials: true });
+  }
 
-      user = {
-        user: response.user,
-        itineraries: response.itineraries.content,
-        reviews: response.reviews.content
-      }
+  moreItineraries(page: number): Observable<UserDetailsDTO> {
+    return this.httpClient.get<UserDetailsDTO>(`${BASE_URL}/me?pageItineraries=${page}`, { withCredentials: true });
+  }
 
-      for (let page = 0; page < pages; page++) {
-        this.httpClient.get<UserDetailsDTO>(`${BASE_URL}/me?itineraryPage=${page}&reviewPage=${page}`, { responseType: "json" }).subscribe((response) => {
-          response.itineraries.content.forEach((i) => {user.itineraries.push(i)});
-          response.reviews.content.forEach((r) => {user.reviews.push(r)});
-        });
-      }
-      return user;
-    });
+  moreReviews(page: number): Observable<UserDetailsDTO> {
+    return this.httpClient.get<UserDetailsDTO>(`${BASE_URL}/me?pageReviews=${page}`, { withCredentials: true });
   }
 
 }
