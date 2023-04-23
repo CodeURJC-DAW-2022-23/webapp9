1afe8bb94e9616e35877215fbeb68a8b5779d039
xIvqn <xivqn@proton.me>
2023-04-23 14:36:49 +0200

    Feat: Get search query parameters from url
    Co-authored-by: Adaya:) <103607990+AdayaUwU@users.noreply.github.com>

--- frontend/angularclient/src/app/components/search/search.component.ts
+++ frontend/angularclient/src/app/components/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Information } from 'src/app/models/information.model';
 
 @Component({
   selector: 'app-search',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent {
+
+  name!: string;
+  type!: string;
+  sort!: string;
+  order!: string;
+  page!: number;
+  items: Information[] = [];
+
+  constructor (private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.activatedRoute.queryParams.subscribe((params: Params) => {
+      this.name = params['name'] ? params['name'] : "";
+      this.type = params['type'] ? params['type'] : "itinerary";
+      this.sort = params['sort'] ? params['sort'] : "id";
+      this.order = params['order'] ? params['order'] : "DESC";
+      this.page = params['page'] ? params['page'] : "0";
+    });
+  }
 
 }
