a9fb98d5df1869abf3f439cf3836df6517911ddb
AdayaUwU <am.ruiz.2020@alumnos.urjc.es>
2023-04-23 15:19:10 +0200

    Feat: Results image and pagination

--- frontend/angularclient/src/app/components/search/search.component.ts
+++ frontend/angularclient/src/app/components/search/search.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Information } from 'src/app/models/information.model';
 import { DestinationService } from 'src/app/services/destination.service';
+import { InformationService } from 'src/app/services/information.service';
 import { ItineraryService } from 'src/app/services/itinerary.service';
 import { PlaceService } from 'src/app/services/place.service';
 
@@ -18,11 +19,14 @@ export class SearchComponent {
   order!: string;
   page!: number;
   items: Information[] = [];
+  service!: InformationService;
+  loader: boolean = false;
 
   constructor(private activatedRoute: ActivatedRoute,
-    destinationService: DestinationService,
-    itineraryService: ItineraryService,
-    placeService: PlaceService) { }
+    private destinationService: DestinationService,
+    private itineraryService: ItineraryService,
+    private placeService: PlaceService) {
+  }
 
   ngOnInit() {
     this.activatedRoute.queryParams.subscribe((params: Params) => {
@@ -31,7 +35,37 @@ export class SearchComponent {
       this.sort = params['sort'] ? params['sort'] : "id";
       this.order = params['order'] ? params['order'] : "DESC";
       this.page = params['page'] ? params['page'] : "0";
+      if (this.type == 'destination') {
+        this.service = this.destinationService
+      } else if (this.type == 'itinerary') {
+        this.service = this.itineraryService
+      } else if (this.type == 'place') {
+        this.service = this.placeService
+      }
     });
+
   }
 
+  getImage(i: Information): string {
+    return this.service.getImage(i)
+  }
+
+  loadMore() {
+    this.loader = true;
+    this.page += 1;
+    this.service.search(this.name, this.type, this.sort, this.order, this.page).subscribe(
+      response => {
+        response.content.forEach(item => {
+          this.items.push(item);
+        });
+        this.loader = false;
+      },
+      error => {
+        console.log(error);
+        this.loader = false;
+      }
+    )
+  }
 }
+
+
