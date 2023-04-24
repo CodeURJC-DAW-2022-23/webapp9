a4ba3398f06ec977f0836c14325f962cecf29989
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2023-04-24 23:18:20 +0200

    Fix: Updated SPAController

--- backend/src/main/java/com/tripscanner/TripScanner/controller/webController/SPAController.java
+++ backend/src/main/java/com/tripscanner/TripScanner/controller/webController/SPAController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SPAController {
-    @GetMapping("/new/**/{path:[^\\.]*}")
+    @GetMapping({"/new/**/{path:[^\\.]*}", "/{path:new[^\\.]*}"})
     public String redirect() {
         return "forward:/new/index.html";
     }
